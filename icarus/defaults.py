07aff448e8212bd570379788e137d1d5c763e37c
Thomas Ip <hkmp7tommy@gmail.com>
2019-04-08 22:22:37 +0800

    Implement post permalink resolution.

---
+++ icarus/defaults.py
@@ -0,0 +1,3 @@
+# The default permalink for posts. The date is arranged as such to allow
+# the files to be sorted normally in the file system.
+PERMALINK = '/:year/:month/:day/:title'
--- icarus/generator.py
+++ icarus/generator.py
@@ -1,5 +1,6 @@
 import os
 import re
+import shutil
 
 import mistune
 import yaml
@@ -7,8 +8,15 @@ from jinja2 import Environment, FileSystemLoader
 from jinja2.ext import Extension
 
 import utils
+from defaults import PERMALINK
 
 OUTPUT_DIR = 'dist'
+
+
+class FormatError(Exception):
+    """ Raised when the format of a template is malformed.
+    """
+    pass
 
 
 class FrontMatterExtension(Extension):
@@ -51,33 +59,73 @@ class Generator:
         with open('config.yaml', 'r') as f:
             self.context['config'] = yaml.load(f.read())
 
-    def _resolve_permalink(self, permalink, filename):
-        pass
+    def _resolve_post_permalink(self, permalink, filename):
+        """ Resolves a permalink against a filename and its frontmatter and
+        return a concrete path.
+
+        For example, given a post with filename `2019-04-08-hello-world.md`
+        with a frontmatter entry of `category: tech`, a permalink of
+        `permalink: /blog/:category/:year/:month/:title` resolves as the
+        path `/blog/tech/2019/04/hello-world.html`.
+        This expects the filename to always be in the format
+        `yyyy-mm-dd-title(-title)*.md`.
+        """
+        path = []
+        category = self.frontmatters[filename].get('category')
+        year, month, day, *titles = filename.replace('.md', '').split('-')
+        for segment in permalink[1:].split('/'):
+            if segment.startswith(':'):
+                placeholder = segment[1:]
+                if placeholder == 'year':
+                    path.append(year)
+                elif placeholder == 'month':
+                    path.append(month)
+                elif placeholder == 'day':
+                    path.append(day)
+                elif placeholder == 'category':
+                    if category:
+                        path.append(category)
+                    else:
+                        raise FormatError('Frontmatter does not contain a \
+                            category variable but the permalink calls for it.')
+                elif placeholder == 'title':
+                    path.append('-'.join(titles))
+                else:
+                    raise FormatError(
+                        'Unknown placeholder `{}`.'.format(placeholder))
+            else:
+                path.append(segment)
+
+        return os.path.join(*path) + '.html'
 
     def _render_posts(self):
         markdown_renderer = mistune.Markdown()
+        de_permalink = self.context['config'].get('permalink', PERMALINK)
         for post in os.listdir('posts'):
-            #with open(os.path.join('posts', post), 'r') as f:
-            #    file_content = f.read()
-            #jinja_markdown = self.env.from_string(file_content)
-            jinja_markdown = self.env.get_template(post)
-            markdown = jinja_markdown.render(
-                config=self.context['config'], **self.frontmatters[post])
-            content = markdown_renderer(markdown)
-            self.frontmatters[post]['content'] = content
+            jinjadown = self.env.get_template(post)
+            markdown = jinjadown.render(config=self.context['config'],
+                                        **self.frontmatters[post])
+            html = markdown_renderer(markdown)
+            self.frontmatters[post]['content'] = html
             output = self.env.get_template('post.html').render(
                 post=self.frontmatters[post], config=self.context['config'])
-            #_url = self._resolve_permalink(self.frontmatters[post].get('permalink'))
-            self._write_to_dist(post, output)
+            url = self._resolve_post_permalink(
+                self.frontmatters['post.html'].get('permalink', de_permalink),
+                post)
+            self._write_to_dist(url, output)
 
     @staticmethod
     def _write_to_dist(path, content):
-        with open(os.path.join(OUTPUT_DIR, path), 'w') as f:
+        full_path = os.path.join(OUTPUT_DIR, path)
+        if not os.path.exists(os.path.dirname(full_path)):
+            os.makedirs(os.path.dirname(full_path))
+        with open(full_path, 'w') as f:
             f.write(content)
 
     def generate(self):
         if os.path.exists(OUTPUT_DIR):
-            os.removedirs(OUTPUT_DIR)
+            shutil.rmtree(OUTPUT_DIR)
         os.makedirs(OUTPUT_DIR)
         self._load_config()
         self._render_posts()
+        self.context['posts'] = self.frontmatters()
